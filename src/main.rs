81922d5c97c2834cbc45f4bd1fad1c2c44dd0fc0
brokecore <reversecel@tuta.com>
2025-12-28 18:12:19 -0600

    added new gamemodes and cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -7,55 +7,56 @@ pub fn greetings(){
     // rules & welcome message
     println!("Hello, welcome to the guessing game");
     println!("Here are the rules:");
-    println!("1. You have 5 chances to guess the correct number");
+    println!("1. You have 3(hard)-10(easy) chances to guess the correct number");
     println!("2. The difficulty either increases or decreases your chances");
     println!("3. The number you pick is inbetween 1-100");
 }
 
-
-
 pub fn menu(){ //cli menu
-    let options = vec!["easy", "medium (doesnt work)", "hard (doesnt work)"];
-    let choices = Select::new("please pick you gamemode.", options)
+    let options = vec!["easy", "medium", "hard"];
+    let choices = Select::new("please pick
+     you gamemode.", options)
         .prompt()
         .unwrap();
 
     match choices {
-        "easy" => gameeasy(),
+        "easy" => game(10),
+        "medium" => game(5),
+        "hard" => game(3),
         _ => {}
     }
 }
 
-fn gameeasy(){
-    for i in 0..10 { //stops after 10 tries
+fn game(chances: u8){
         let mut rng = rand::rng();
-        let number: u8 = rng.random_range(1..100);
+        let number: u8 = rng.random_range(1..100); // the random number
 
-        println!("You have 10 chances.");
-        println!("Please insert your number here:");
+        println!("You have {} chances.", chances);
 
-        let mut guess = String::new();
+        for i in 0..chances {
+            println!("Please insert your number here:");
 
-        let _ = io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read guess");
+            let mut guess = String::new();
 
-        let guess: u8 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("enter a number.");
+            let _ = io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read guess");
+
+            let guess: u8 = match guess.trim().parse() {
+                Ok(num) => num,
+              Err(_) => {
+                println!("enter a valid number (1-100.)");
                 continue;
             }
         };
-
         if guess == number.into() {
             println!("you win!");
-            break
+            return; //exit game if won
         } else {
-            println!("wrong") //TODO: add different message after loop break
-        };
-        
+            println!("wrong")
+        }
     }
+    println!("the corrent number was {}", number);
 }
 
 fn main() {
